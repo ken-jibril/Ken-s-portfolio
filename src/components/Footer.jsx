dec68d42cdb1ff1422bc1edef0d0c923e9efd5d4
ken kagai <jibken80@gmail.com>
2025-09-27 15:33:48 +0300

    add a footer in the component folder and a text.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return ( 
+        <div className="">
+            <p className="bg-black text-2xl py-1 w-full">&cpoy; Made by Ken. 2025</p>
+        </div>
+     );
+}
+
+export default Footer;
