43fe0a8d39efe6a2c1a239c935e56d97290cdbd7
ken kagai <jibken80@gmail.com>
2025-09-30 20:41:26 +0300

    import the footer component, wrap the whole App.jsx with a div to make the footer sit at the bottom of every page rendered.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './Pages/Home'
 import About from './Pages/About'
 import Projects from './Pages/Projects'
 import Contact from './Pages/Contact'
+import Footer from './components/Footer'
 
 
 function App() {
@@ -11,16 +12,23 @@ function App() {
 
   return (
     <>
+    <div className="flex flex-col min-h-screen">
       <BrowserRouter>
        <Navbar />
        
-       <Routes>
-        <Route   path='/' element={<Home />} />
-        <Route  path='/about' element={<About />} />
-        <Route  path='/project' element={<Projects />} />
-        <Route  path='/contact' element={<Contact />} />
-       </Routes>
-      </BrowserRouter>
+
+       <div className="flex-grow">
+        <Routes>
+          <Route   path='/' element={<Home />} />
+          <Route  path='/about' element={<About />} />
+          <Route  path='/project' element={<Projects />} />
+          <Route  path='/contact' element={<Contact />} />
+        </Routes>
+       </div>
+
+       <Footer />
+      </BrowserRouter>  
+    </div>
     </>
   )
 }
