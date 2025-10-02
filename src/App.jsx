ebe04e79db30730999da9e85133918c8bcffa2c5
ken kagai <jibken80@gmail.com>
2025-10-02 15:43:15 +0300

    refactor: fixe a typo for importation of projects component

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
         <Routes>
           <Route   path='/' element={<Home />} />
           <Route  path='/about' element={<About />} />
-          <Route  path='/project' element={<Projects />} />
+          <Route  path='/projects' element={<Projects />} />
           <Route  path='/contact' element={<Contact />} />
         </Routes>
        </div>
